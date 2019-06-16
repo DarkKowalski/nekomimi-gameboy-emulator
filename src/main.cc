fc3fa25e3097e978f0152c3c1516fb19b35a82c5
Marshmallow <xiongnemo@126.com>
2019-06-16 16:18:18 +0800

    format main

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,6 @@
 #include "motherboard.h"
 //#define DEBUG
 
-
 using namespace gameboy;
 
 int main(int argc, char *argv[])
@@ -18,13 +17,13 @@ int main(int argc, char *argv[])
     //CPU Cycle
     motherboard.loop();
 
-    #ifdef DEBUG
+#ifdef DEBUG
     FILE *out_ram = fopen("out_ram.gbram", "w+b");
     fwrite(motherboard.mem.memory_byte, sizeof(uint8_t), 0xFFFF, out_ram);
     fclose(out_ram);
     printf("Memory written to out_ram.gbram.\n");
     out_ram = nullptr;
-    #endif
+#endif
     // quit
     motherboard.form.destroy_window();
 
